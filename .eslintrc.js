274694824d55213fcb542f6dfb2da699be2ede2b
Héctor Soler <hecalana@gmail.com>
2024-10-14 13:13:26 -0400

    Disable eslint dot notation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,30 @@
 module.exports = {
   "root": true,
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-      "eslint:recommended",
-      "plugin:@angular-eslint/recommended",
-      "standard"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 15
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "@angular-eslint",
-        "standard",
-        "promise",
-        "import",
-        "node"
-    ],
-    "ignorePatterns": ['dist', '.eslintrc.js'],
-    "rules": {}
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@angular-eslint/recommended",
+    "standard"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 15
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "@angular-eslint",
+    "standard",
+    "promise",
+    "import",
+    "node"
+  ],
+  "ignorePatterns": ['dist', '.eslintrc.js'],
+  "rules": {
+    'dot-notation': 'off',
+  }
 };
 
